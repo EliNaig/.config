9d345862382fa120f90d12438854def5818020f1
EliNaig <120431229+EliNaig@users.noreply.github.com>
2024-02-10 10:53:29 -0600

    Update options.lua

--- nvim/lua/neotex/core/options.lua
+++ nvim/lua/neotex/core/options.lua
@@ -56,18 +56,3 @@ local options = {
 for k, v in pairs(options) do
   vim.opt[k] = v
 end
-
--- CLIPBOARD -- (for yanky)
--- May help Arch/Debian Linux users
--- vim.g.clipboard = {
---   name = "xsel_override",
---   copy = {
---     ["+"] = "xsel --input --clipboard",
---     ["*"] = "xsel --input --primary",
---   },
---   paste = {
---     ["+"] = "xsel --output --clipboard",
---     ["*"] = "xsel --output --primary",
---   },
---   cache_enabled = 1,
--- }
